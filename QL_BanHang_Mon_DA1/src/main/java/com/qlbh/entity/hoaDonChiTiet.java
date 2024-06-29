f2f5b444dddf49ea2a9768162d956aa1d9d00d04
thanhdp05 <Thanhphuocdinh05@gmail.com>
2024-06-29 13:41:45 +0700

    create entity class

---
+++ QL_BanHang_Mon_DA1/src/main/java/com/qlbh/entity/hoaDonChiTiet.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.qlbh.entity;
+
+/**
+ *
+ * @author ASUS
+ */
+public class hoaDonChiTiet {
+    int maHD ;
+    String maSP ;
+    int soLuong ;
+    double donGia ;
+
+    public hoaDonChiTiet() {
+    }
+
+    public hoaDonChiTiet(int maHD, String maSP, int soLuong, double donGia) {
+        this.maHD = maHD;
+        this.maSP = maSP;
+        this.soLuong = soLuong;
+        this.donGia = donGia;
+    }
+
+    public int getMaHD() {
+        return maHD;
+    }
+
+    public void setMaHD(int maHD) {
+        this.maHD = maHD;
+    }
+
+    public String getMaSP() {
+        return maSP;
+    }
+
+    public void setMaSP(String maSP) {
+        this.maSP = maSP;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public double getDonGia() {
+        return donGia;
+    }
+
+    public void setDonGia(double donGia) {
+        this.donGia = donGia;
+    }
+    
+}
